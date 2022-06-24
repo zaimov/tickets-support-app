e8265d7a73aabfa5fabf0cfc82608fa066f45940
Denis Zaimov <denislav.zaimov@gmail.com>
2022-06-24 14:50:54 -0400

    added tickets routes to create a ticket, get all tickets, created a ticket model

---
+++ backend/routes/ticketRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const {getTickets, createTicket} = require('../controllers/ticketController')
+
+const {protect} = require('../middleware/authMiddleware')
+
+router.get('/', protect, getTickets)
+router.post('/', protect, createTicket)
+
+
+module.exports = router
